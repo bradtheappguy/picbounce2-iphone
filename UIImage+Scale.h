6640304fb3154a0887eb5f94db5ef11365f461cb
Tom DiZoglio <tomdiz@yahoo.com>
2011-11-21 17:00:26 -0800

    resize checkin

---
+++ UIImage+Scale.h
@@ -0,0 +1,13 @@
+//
+//  NSObject+UIImage_Scale_h.h
+//  PicBounce2
+//
+//  Created by Thomas DiZoglio on 11/14/11.
+//  Copyright (c) 2011 Clixtr, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (UIImage_Scale_h)
+
+@end
