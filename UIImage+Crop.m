dae3e6a78246f99635d83f9c79dac851c0dbe22a
Brad Smith <bradsmithinc@gmail.com>
2011-11-27 16:31:12 -0800

    [Finished #20767171] iPad photo not square cropped.

---
+++ UIImage+Crop.m
@@ -0,0 +1,13 @@
+//
+//  UIImage+Crop.m
+//  PicBounce2
+//
+//  Created by Brad Smith on 11/27/11.
+//  Copyright (c) 2011 Clixtr, Inc. All rights reserved.
+//
+
+#import "UIImage+Crop.h"
+
+@implementation UIImage (Crop)
+
+@end
